e8e7cb127b0d120ff48c2eaf43be3d15e6cb3f16
amon <amon@nandynarwhals.org>
2016-08-20 04:58:44 +0800

    Test privesca

---
+++ services/6-privesca/dockerrun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -dt -p 1342:31337 privesca
---
+++ services/6-privesca/escalate.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int check(char *user_pass, char *pass) {
+    int index = 0;
+    while (user_pass[index] != 0) {
+        if (user_pass[index] != pass[index]) {
+            return 0;
+        }
+        index++;
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    char user_pass[256];
+    printf("Password: ");
+    scanf("%255s", user_pass);
+    if (check(user_pass, "ThisIsALongPassword")) {
+        printf("Win!\n");
+        system("/bin/sh");
+    }
+    else {
+        printf("Fail!\n");
+    }
+}
